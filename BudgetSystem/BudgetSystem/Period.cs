922871bab9d964501a33f13e6d1cdb0a2d13e257
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 22:02:55 +0800

    refactor: use another.Start and End, instead of budget firstDay and lastDay

--- BudgetSystem/BudgetSystem/Period.cs
+++ BudgetSystem/BudgetSystem/Period.cs
@@ -20,8 +20,8 @@ namespace BudgetSystem
         public int OverlappingDays(Budget budget)
         {
             var another = new Period(budget.FirstDay(), budget.LastDay());
-            var firstDay = budget.FirstDay();
-            var lastDay = budget.LastDay();
+            var firstDay = another.Start;
+            var lastDay = another.End;
             var overlappingStart = Start > firstDay ? Start : firstDay;
             var overlappingEnd = End < lastDay ? End : lastDay;
 
