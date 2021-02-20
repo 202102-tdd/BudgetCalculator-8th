e2c99636939d87e1cf035955591d4ff067d68b7d
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 22:09:39 +0800

    refactor: move invalid period into OverlappingDays()

--- BudgetSystem/BudgetSystem/Period.cs
+++ BudgetSystem/BudgetSystem/Period.cs
@@ -19,6 +19,11 @@ namespace BudgetSystem
 
         public int OverlappingDays(Period another)
         {
+            if (Start > End)
+            {
+                return 0;
+            }
+
             var overlappingStart = Start > another.Start ? Start : another.Start;
             var overlappingEnd = End < another.End ? End : another.End;
 
