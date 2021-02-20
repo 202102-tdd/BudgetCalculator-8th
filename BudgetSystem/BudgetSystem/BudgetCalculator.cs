3c29acdf95985800010ba8c75eec01e819dc17b2
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 22:10:48 +0800

    refactor: clean up BudgetCalculator.cs

--- BudgetSystem/BudgetSystem/BudgetCalculator.cs
+++ BudgetSystem/BudgetSystem/BudgetCalculator.cs
@@ -1,7 +1,6 @@
 ﻿#region
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 #endregion
@@ -21,13 +20,6 @@ namespace BudgetSystem
         {
             var period = new Period(start, end);
             return _repo.GetAll().Sum(b => b.OverlappingAmount(period));
-        }
-
-        private static decimal GetMonthAmount(DateTime start, List<Budget> budgets)
-        {
-            var startMonthData = budgets.FirstOrDefault(a => a.YearMonth == start.ToString("yyyyMM"));
-
-            return startMonthData?.Amount ?? 0;
         }
     }
 }
