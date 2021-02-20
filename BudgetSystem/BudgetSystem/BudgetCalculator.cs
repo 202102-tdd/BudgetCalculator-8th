cc8dcc55214ce173239899b5a55050a6beff2d97
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 22:01:13 +0800

    refactor: clean up

--- BudgetSystem/BudgetSystem/BudgetCalculator.cs
+++ BudgetSystem/BudgetSystem/BudgetCalculator.cs
@@ -8,26 +8,6 @@ using System.Linq;
 
 namespace BudgetSystem
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime Start { get; private set; }
-        public DateTime End { get; private set; }
-
-        public int OverlappingDays(Budget budget)
-        {
-            var overlappingStart = Start > budget.FirstDay() ? Start : budget.FirstDay();
-            var overlappingEnd = End < budget.LastDay() ? End : budget.LastDay();
-
-            return (overlappingEnd - overlappingStart).Days + 1;
-        }
-    }
-
     public class BudgetCalculator
     {
         private readonly IBudgetRepo _repo;
---
+++ BudgetSystem/BudgetSystem/Period.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace BudgetSystem
+{
+    public class Period
+    {
+        public Period(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        private DateTime Start { get; }
+        private DateTime End { get; }
+
+        public int OverlappingDays(Budget budget)
+        {
+            var overlappingStart = Start > budget.FirstDay() ? Start : budget.FirstDay();
+            var overlappingEnd = End < budget.LastDay() ? End : budget.LastDay();
+
+            return (overlappingEnd - overlappingStart).Days + 1;
+        }
+    }
+}
