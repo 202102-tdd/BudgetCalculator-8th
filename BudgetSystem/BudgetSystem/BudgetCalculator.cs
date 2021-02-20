a0d72dd18429cecce430cfefcf6c6cde457c17b9
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 22:09:00 +0800

    refactor: use Sum() instead of foreach loop

--- BudgetSystem/BudgetSystem/BudgetCalculator.cs
+++ BudgetSystem/BudgetSystem/BudgetCalculator.cs
@@ -25,15 +25,10 @@ namespace BudgetSystem
             }
 
             var budgets = _repo.GetAll();
-            // if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
-            // {
-            //     var interval = (end - start).Days + 1;
-            //     var startAmount = GetMonthAmount(start, budgets);
-            //     return interval * startAmount / DateTime.DaysInMonth(end.Year, end.Month);
-            // }
 
             decimal middleAmount = 0;
             var period = new Period(start, end);
+            return budgets.Sum(b => b.OverlappingAmount(period));
             foreach (var budget in budgets)
             {
                 middleAmount += budget.OverlappingAmount(period);
